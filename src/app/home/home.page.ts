37cc40ba0c8179d361fe560094a0c30f778d3cb4
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-02-01 13:47:45 +0100

    Municipios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as moment from 'moment';
+//npm install moment
 import { ApiServiceProvider } from '../api/api-service';
-import { EstadoCielo, InterfaceMunicipio, PrevisionTiempo } from '../model/PrevisionTiempo';
+import { PrevisionPresentacion } from '../model/PrevisionPresentacion';
+import { InterfaceMunicipio, PrevisionTiempo } from '../model/PrevisionTiempo';
 
 @Component({
   selector: 'app-home',
@@ -8,42 +12,85 @@ import { EstadoCielo, InterfaceMunicipio, PrevisionTiempo } from '../model/Previ
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
-  estadosCielo:Array<EstadoCielo> = new Array();
-  municipio = "41091"
-  municipios:InterfaceMunicipio[]=new Array;
-  constructor(private apiService: ApiServiceProvider) {
-  }
-  ngOnInit(): void {
-    this.getPrevisionMunicipios();
-    this.getAllMunicipios();
-}
-  getPrevisionMunicipios():void{
-    this.apiService.getPrevisionDiariaMunicipio(this.municipio).then((data:PrevisionTiempo[]) =>{
-      //prevision del tiempo de hoy
-      //se tiene que poner ? para prevenir que es null, ya que la interfaz no tiene un constructor
-      this.estadosCielo?.push(data[0].prediccion.dia[0].estadoCielo[0]);
-      this.estadosCielo?.push(data[0].prediccion.dia[0].estadoCielo[4]);
-      this.estadosCielo?.push(data[0].prediccion.dia[0].estadoCielo[5]);
-      this.estadosCielo?.push(data[0].prediccion.dia[0].estadoCielo[6]);
-      //Prevision del tiempo mañana
-      this.estadosCielo?.push(data[0].prediccion.dia[1].estadoCielo[3]);
-      this.estadosCielo?.push(data[0].prediccion.dia[1].estadoCielo[4]);
-      this.estadosCielo?.push(data[0].prediccion.dia[1].estadoCielo[5]);
-      this.estadosCielo?.push(data[0].prediccion.dia[1].estadoCielo[6]);
-    }).catch((error: string) => {
-      console.log(error);
+  public listaPrevisiones: Array<PrevisionPresentacion> = new Array();
+  public municipio: InterfaceMunicipio;
+
+  constructor(private apiService: ApiServiceProvider, private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.municipio = JSON.parse(params["municipio"]);
     });
   }
-  getAllMunicipios():void{
-    this.apiService.getMunicipios().then((data:InterfaceMunicipio[] ) =>{
-      //console.log(data);
-      for (var i =0; i<data.length;i++){
-        this.municipios?.push(data[i]);
-      }
-        console.log(this.municipios);
-    }).catch((error:string)=>{
-      console.log(error);
-    })
-  }
 
+  ngOnInit(): void {
+    this.apiService.getPrevisionDiariaMunicipio(this.municipio.CODPROVINCIA + this.municipio.CODMUNICIPIO)
+      .then((data: PrevisionTiempo[]) => {
+        let fechaHoraActual = moment(new Date());
+        fechaHoraActual.locale('es');
+        let horaActual: number = fechaHoraActual.hour();
+        //previsión para hoy
+        //solo añado la previsión de los intervalos horarios superiores
+        //a la hora actual
+        if (horaActual < Number.parseInt(data[0].prediccion.dia[0].estadoCielo[3].periodo.substring(3, 5))) {
+          this.listaPrevisiones.push(    //intervalo de las 0 a las 6
+            new PrevisionPresentacion('hoy',
+              data[0].prediccion.dia[0].estadoCielo[3].periodo,
+              data[0].prediccion.dia[0].estadoCielo[3].value,
+              data[0].prediccion.dia[0].estadoCielo[3].descripcion,
+            ));
+        }
+        if (horaActual < Number.parseInt(data[0].prediccion.dia[0].estadoCielo[4].periodo.substring(3, 5))) {
+          this.listaPrevisiones.push(    //intervalo de las 6 a las 12
+            new PrevisionPresentacion('hoy',
+              data[0].prediccion.dia[0].estadoCielo[4].periodo,
+              data[0].prediccion.dia[0].estadoCielo[4].value,
+              data[0].prediccion.dia[0].estadoCielo[4].descripcion,
+            ));
+        }
+        if (horaActual < Number.parseInt(data[0].prediccion.dia[0].estadoCielo[5].periodo.substring(3, 5))) {
+          this.listaPrevisiones.push(    //intervalo de las 12 a las 18 
+            new PrevisionPresentacion('hoy',
+              data[0].prediccion.dia[0].estadoCielo[5].periodo,
+              data[0].prediccion.dia[0].estadoCielo[5].value,
+              data[0].prediccion.dia[0].estadoCielo[5].descripcion,
+            ));
+        }
+        if (horaActual < Number.parseInt(data[0].prediccion.dia[0].estadoCielo[6].periodo.substring(3, 5))) {
+          this.listaPrevisiones.push(    //intervalo de las 18 a las 24
+            new PrevisionPresentacion('hoy',
+              data[0].prediccion.dia[0].estadoCielo[6].periodo,
+              data[0].prediccion.dia[0].estadoCielo[6].value,
+              data[0].prediccion.dia[0].estadoCielo[6].descripcion,
+            ));
+        }
+
+        //previsión para mañana
+        this.listaPrevisiones.push(    //intervalo de las 0 a las 6
+          new PrevisionPresentacion('mañana',
+            data[0].prediccion.dia[1].estadoCielo[3].periodo,
+            data[0].prediccion.dia[1].estadoCielo[3].value,
+            data[0].prediccion.dia[1].estadoCielo[3].descripcion,
+          ));
+        this.listaPrevisiones.push(    //intervalo de las 6 a las 12
+          new PrevisionPresentacion('mañana',
+            data[0].prediccion.dia[1].estadoCielo[4].periodo,
+            data[0].prediccion.dia[1].estadoCielo[4].value,
+            data[0].prediccion.dia[1].estadoCielo[4].descripcion,
+          ));
+        this.listaPrevisiones.push(    //intervalo de las 12 a las 18
+          new PrevisionPresentacion('mañana',
+            data[0].prediccion.dia[1].estadoCielo[5].periodo,
+            data[0].prediccion.dia[1].estadoCielo[5].value,
+            data[0].prediccion.dia[1].estadoCielo[5].descripcion,
+          ));
+        this.listaPrevisiones.push(    //intervalo de las 18 a las 24
+          new PrevisionPresentacion('mañana',
+            data[0].prediccion.dia[1].estadoCielo[6].periodo,
+            data[0].prediccion.dia[1].estadoCielo[6].value,
+            data[0].prediccion.dia[1].estadoCielo[6].descripcion,
+          ));
+      })
+      .catch((error: string) => {
+        console.log(error);
+      });
+  }//end_ngOnInit
 }
--- src/app/municipios/municipios.page.ts
+++ src/app/municipios/municipios.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { ApiServiceProvider } from '../api/api-service';
+import { InterfaceMunicipio } from '../model/PrevisionTiempo';
 
 @Component({
   selector: 'app-municipios',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./municipios.page.scss'],
 })
 export class MunicipiosPage implements OnInit {
+  public municipios: InterfaceMunicipio[] = new Array();
+  public municipiosBuscados: InterfaceMunicipio[] = new Array();
+  public municipioBuscar: string = '';
 
-  constructor() { }
+  constructor(
+    private apiService: ApiServiceProvider,
+    private navCtrl: NavController
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.apiService
+      .getMunicipios()
+      .then((data: InterfaceMunicipio[]) => {
+        this.municipios = data;
+      })
+      .catch((error: string) => {
+        console.log(error);
+      });
+  } //end_ngOnInit
+
+  buscarMunicipio() {
+    if (this.municipioBuscar.trim() == '') {
+      this.municipiosBuscados = new Array();
+      return;
+    }
+    this.municipiosBuscados = new Array();
+    this.municipios.forEach((municipio) => {
+      if (
+        municipio.MUNICIPIO.toUpperCase().startsWith(
+          this.municipioBuscar.trim().toUpperCase()
+        )
+      ) {
+        this.municipiosBuscados.push(municipio);
+      }
+    });
   }
 
+  municipioClick(municipio: InterfaceMunicipio) {
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        municipio: JSON.stringify(municipio),
+      },
+    };
+    this.navCtrl.navigateForward('/home', navigationExtras);
+  }
 }
